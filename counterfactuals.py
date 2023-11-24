1f1969441f40ef4b6af99f79c2a243b17b4c7678
jedalvin <147890767+jedalvin@users.noreply.github.com>
2023-11-24 10:48:24 +0800

    Create counterfactuals.py

---
+++ counterfactuals.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import os
+import bcrypt
+from hashlib import sha256
+from io import StringIO
+
+# Main app
+def main_app():
+    # Display an image
+    image_path = '/Users/jedalvin/projecthome_cover.png'
+    st.image(image_path)
+    st.markdown("Objective: ")
